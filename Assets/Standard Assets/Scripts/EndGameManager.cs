c6d59408e33bb7e291e9c91f46948636b5a8671d
jaycaz <jacazamias@gmail.com>
2014-11-09 10:52:37 -0600

    Add upgrade buttons to end of level menu

--- Assets/Standard Assets/Scripts/EndGameManager.cs
+++ Assets/Standard Assets/Scripts/EndGameManager.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class EndGameManager : MonoBehaviour {
 
@@ -8,15 +9,32 @@ public class EndGameManager : MonoBehaviour {
     public Texture endLevelTexture;
     public Texture goodUpgradesTexture;
     public Texture badUpgradesTexture;
+    public Texture lightningUpTexture;
+    public Texture sizeUpTexture;
+    public Texture healthUpTexture;
+    public Texture cloudsUpTexture;
+    public Texture speedUpTexture;
+    public Texture turnSpeedUpTexture;
 
+    
     Rect menuButtonRect;
     Rect continueButtonRect;
     Rect goodTabRect;
     Rect badTabRect;
 
+    // Bad upgrades
+    Rect lightningUpRect;
+    Rect sizeUpRect;
+
+    // Good upgrades
+    Rect healthUpRect;
+    Rect cloudsUpRect;
+    Rect speedUpRect;
+    Rect turnSpeedUpRect;
+
 	// Use this for initialization
 	void Start () {
-        showEndLevelScreen = false;
+        showEndLevelScreen = true;
         showGoodTab = true;
         CursorBehaviour c = GameObject.FindObjectOfType<CursorBehaviour>();
         c.CursorLeftClick += OnCursorLeftClick;
@@ -47,6 +65,11 @@ public class EndGameManager : MonoBehaviour {
         {
             showGoodTab = false;
         }
+
+        // Check upgrade buttons
+        if(lightningUpRect.Contains(e.position))
+        {
+        }
     }
 
     void OnGUI()
@@ -55,23 +78,45 @@ public class EndGameManager : MonoBehaviour {
 
         if(showEndLevelScreen)
         {
+            // Main menu rects
             Rect endLevelRect = new Rect(0, 0, Screen.width * (2f / 3f) , Screen.height - 20);
             Rect upgradesRect = new Rect(endLevelRect.xMax + 10, 20, Screen.width - endLevelRect.xMax - 40, Screen.height / 2);
+
+            // Main menu button rects
             menuButtonRect = new Rect(endLevelRect.xMin + endLevelRect.width * (1f / 10f), endLevelRect.yMin + endLevelRect.height * (5.5f / 8f),
                                            endLevelRect.width * (1f / 3f), endLevelRect.height * (1f / 5f));
             continueButtonRect = new Rect(menuButtonRect.xMax + menuButtonRect.xMin, menuButtonRect.yMin, menuButtonRect.width, menuButtonRect.height);
             goodTabRect = new Rect(upgradesRect.xMin, upgradesRect.yMin, upgradesRect.width / 2f, upgradesRect.height / 4f);
             badTabRect = new Rect(goodTabRect.xMax, goodTabRect.yMin, upgradesRect.width - goodTabRect.width, goodTabRect.height);
+
+            // Bad upgrade rects
+            lightningUpRect = new Rect(upgradesRect.xMin + upgradesRect.width * (3f / 32f), 
+                goodTabRect.yMax + upgradesRect.height * (1f/16f), upgradesRect.width / 6, upgradesRect.height / 6);
+            sizeUpRect = new Rect(lightningUpRect.xMax + lightningUpRect.width / 4, lightningUpRect.yMin, lightningUpRect.width, lightningUpRect.height);
+
+
+            // Good upgrade rects
+            healthUpRect = lightningUpRect;
+            cloudsUpRect = sizeUpRect;
+            speedUpRect = new Rect(sizeUpRect.xMax + sizeUpRect.width / 4, sizeUpRect.yMin, sizeUpRect.width, sizeUpRect.height);
+            turnSpeedUpRect = new Rect(speedUpRect.xMax + speedUpRect.width / 4, speedUpRect.yMin, speedUpRect.width, speedUpRect.height);
             
             GUI.DrawTexture(endLevelRect, endLevelTexture);
             
+            // Draw whichever upgrades are visible
             if (showGoodTab)
             {
                 GUI.DrawTexture(upgradesRect, goodUpgradesTexture);
+                GUI.DrawTexture(healthUpRect, healthUpTexture);
+                GUI.DrawTexture(cloudsUpRect, cloudsUpTexture);
+                GUI.DrawTexture(speedUpRect, speedUpTexture);
+                GUI.DrawTexture(turnSpeedUpRect, turnSpeedUpTexture);
             }
             else
             {
                 GUI.DrawTexture(upgradesRect, badUpgradesTexture);
+                GUI.DrawTexture(lightningUpRect, lightningUpTexture);
+                GUI.DrawTexture(sizeUpRect, sizeUpTexture);
             }
 
             // Display boxes capturing mouse input
